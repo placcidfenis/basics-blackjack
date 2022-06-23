2ca90e3b04b138b84398e13bcf3a293657e1e7e7
placcidfenis <timothytan94@gmail.com>
2022-06-23 21:38:38 +0800

    added multiplayer functionality

--- script.js
+++ script.js
@@ -1,39 +1,57 @@
-var gameMode = "waiting to start";
+var gameMode = "no of players";
+var gameStart = false;
 var roundScores = [];
 var playerscore = 0;
 var dealerscore = 0;
 var playercards = [];
 var dealercards = [];
+var playerNo = 1;
+var noOfPlayers = 0;
+
 var main = function (input) {
   var deck = createDeck();
   var shuffledDeck = shuffleDeck(deck);
 
-  if (gameMode == "waiting to start") {
-    gameMode = "Player Draw Cards";
-    myOutputValue = `Player 1, hit submit to draw cards`;
-  } else if (gameMode == "Player Draw Cards") {
+  if (gameMode == "no of players") {
+    if (input < 1 || isNaN(input)) {
+      myOutputValue = `Need at least 1 player to start.`;
+    } else {
+      myOutputValue = `Game started with ${input} players and Dealer.`;
+      gameMode = "New Player Draw Cards";
+      noOfPlayers = input;
+    }
+  } else if (playerNo <= noOfPlayers && gameMode == "New Player Draw Cards") {
     var playercards = [];
     var playerCard1 = shuffledDeck.pop();
     var playerCard2 = shuffledDeck.pop();
     playercards.push(playerCard1);
     playercards.push(playerCard2);
     playerscore = cardScoreCount(playercards);
-    myOutputValue = `Player 1 drew ${playerCard1.name} and ${playerCard2.name}<br>Total Score :${playerscore}. Hit or Stay? `;
+    myOutputValue = `Player ${playerNo} drew ${playerCard1.name} and ${playerCard2.name}<br>Total Score :${playerscore}. Hit or Stay? `;
     var roundscore = {
       score: playerscore,
-      player: "Player",
+      player: `Player ${playerNo}`,
     };
     gameMode = "Player Hit Stay";
-  } else if (gameMode == "Player Hit Stay") {
-    while (input == "hit") {
+  } else if (playerNo <= noOfPlayers && gameMode == "Player Hit Stay") {
+    if (input.toLowerCase() == "hit") {
       var playerCard3 = shuffledDeck.pop();
-      //playercards.push(playerCard3);
       playerscore += playerCard3.value;
-      myOutputValue = `Player 1 drew ${playerCard3.name}. Total Score : ${playerscore} <br>Hit or Stay?`;
+      myOutputValue = `Player ${playerNo} drew ${playerCard3.name}. Total Score : ${playerscore} <br>Hit or Stay?`;
       return myOutputValue;
+    } else if (input.toLowerCase() != "hit" || playerscore > 21) {
+      var roundscore = {
+        score: playerscore,
+        player: `Player ${playerNo}`,
+      };
+      roundScores.push(roundscore);
+      playerNo += 1;
+      gameMode = "New Player Draw Cards";
     }
-    gameMode = "Dealer Draw Cards";
-  } else if (gameMode == "Dealer Draw Cards") {
+    if (playerNo > noOfPlayers) {
+      gameMode = "Dealer Draw Card";
+    }
+  } else if (playerNo > noOfPlayers && gameMode == "Dealer Draw Card") {
     var dealercards = [];
     var dealerCard1 = shuffledDeck.pop();
     var dealerCard2 = shuffledDeck.pop();
@@ -57,19 +75,32 @@ var main = function (input) {
     roundScores.push(roundscore);
   } else if (gameMode == "Decide Winner") {
     console.log(playerscore, dealerscore);
-    if (playerscore < 21 && dealerscore > 21) {
-      myOutputValue = `Player won.`;
-    } else if (playerscore > 21 && dealerscore < 21) {
-      myOutputValue = `Dealer won.`;
-    } else if (playerscore > 21 && dealerscore > 21) {
-      myOutputValue = `Both Player and Dealer have busted, Tie Game.`;
-    } else if (playerscore > dealerscore && playerscore <= 21) {
-      myOutputValue = `Player won.`;
-    } else if (dealerscore > playerscore && dealerscore <= 21) {
-      myOutputValue = `Dealer won`;
-    } else if (playerscore == dealerscore) {
-      myOutputValue = `Both Player and Dealer have same score, Tie Game.`;
+    var winners = winnerCheck(roundScores);
+    console.log(winners);
+    if (winners.length == 0) {
+      myOutputValue = `Tie Game. Both Dealer and Player have bust.`;
+    } else if (winners.length > 1) {
+      var addW = "";
+      for (var i = 1; i < winners.length; i++) {
+        addW += `and ${winners[i].player}`;
+      }
+      myOutputValue = `${winners[0].player} ${addW} have won with ${winners[0].score} points.`;
+    } else {
+      myOutputValue = `${winners[0].player} has won with ${winners[0].score} points.`;
     }
+    // if (playerscore < 21 && dealerscore > 21) {
+    //   myOutputValue = `Player won.`;
+    // } else if (playerscore > 21 && dealerscore < 21) {
+    //   myOutputValue = `Dealer won.`;
+    // } else if (playerscore > 21 && dealerscore > 21) {
+    //   myOutputValue = `Both Player and Dealer have busted, Tie Game.`;
+    // } else if (playerscore > dealerscore && playerscore <= 21) {
+    //   myOutputValue = `Player won.`;
+    // } else if (dealerscore > playerscore && dealerscore <= 21) {
+    //   myOutputValue = `Dealer won`;
+    // } else if (playerscore == dealerscore) {
+    //   myOutputValue = `Both Player and Dealer have same score, Tie Game.`;
+    // }
     gameMode = "waiting to start";
   }
 
@@ -127,18 +158,26 @@ var cardScoreCount = function (cards) {
   return score;
 };
 
-// check for winner | scores is an array of score and name
-// var winningCheck = function (scores) {
-//   highscore = 0;
-//   sorted = scores
-//     .sort(function (a, b) {
-//       return a.score - b.score;
-//     })
-//     .reverse();
-//   for (var i = 0; i < sorted.length; i++) {
-//     if (sorted[i].score > 21) {
-//       sorted.splice(i, 1);
-//     }
-//   }
-//   score;
-//};
+// check for winner | scores is an array of score and name, returns a list of winners
+var winnerCheck = function (scores) {
+  var highscore = 0;
+  var winners = [];
+  sorted = scores
+    .sort(function (a, b) {
+      return a.score - b.score;
+    })
+    .reverse();
+  console.log(sorted);
+  for (var i = 0; i < sorted.length; i++) {
+    if (sorted[i].score > 21) {
+      sorted.splice(i, 1);
+    }
+  }
+  highscore = sorted[0].score;
+  for (var j = 0; j < sorted.length; j++) {
+    if (sorted[j].score == highscore) {
+      winners.push(sorted[j]);
+    }
+  }
+  return winners;
+};
